60249a4e899be00e31d8d934d28ad55e0593f56f
Joe Rourke <jrourke@expediagroup.com>
2023-12-15 12:06:32 +0000

    fix: docker headless and hide outputs

--- server/install.sh
+++ server/install.sh
@@ -13,33 +13,45 @@ done
 echo "All required commands have been installed. Moving on..."
 
 echo "Installing virtualenv and setting up environment"
-pip3 install virtualenv
-python3 -m venv venv
-echo "export commutr_postgres_password=wnGhxi9ZaFN42G" | tee -a venv/bin/activate
-source venv/bin/activate
+{
+  pip3 install virtualenv
+  python3 -m venv venv
+  echo "export commutr_postgres_password=wnGhxi9ZaFN42G" | tee -a venv/bin/activate
+  source venv/bin/activate
+} &> /dev/null
 echo "-- Done"
 
 echo "Installing pip requirements"
-pip install -r requirements.txt
+{
+  pip install -r requirements.txt
+} &> /dev/null
 echo "-- Done"
 
 echo "Pulling Docker images"
-docker pull postgres:latest
-docker pull redis/redis-stack:latest
+{
+  docker pull postgres:latest
+  docker pull redis/redis-stack:latest
+} &> /dev/null
 echo "-- Done"
 
 echo "Starting PostgreSQL and Redis"
-docker run --name commutr-db-2 -e POSTGRES_PASSWORD=$commutr_postgres_password -d -p 5432:5432 postgres
-docker run -p 6379:6379 -it redis/redis-stack:latest
+{
+  docker run --name commutr-db -e POSTGRES_PASSWORD=$commutr_postgres_password -d -p 5432:5432 postgres
+  docker run -p 6379:6379 -d redis/redis-stack:latest
+} &> /dev/null
 echo "-- Done"
 
 echo "Setting up database"
 echo "-- Make Migrations"
-python manage.py makemigrations
+{
+  python manage.py makemigrations
+} &> /dev/null
 echo "---- Done"
 
 echo "-- Do Migrations"
-python manage.py migrate
+{
+  python manage.py migrate
+} &> /dev/null
 echo "---- Done"
 
 echo "Installation has completed. Try running the API with './run.sh'"
